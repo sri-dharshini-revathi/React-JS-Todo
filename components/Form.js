e4458792b96b8f95221e0bbd6bcf342c66f5e883
sri-dharshini-revathi <67547021+sri-dharshini-revathi@users.noreply.github.com>
2021-05-11 19:03:18 +0530

    Add files via upload

---
+++ components/Form.js
@@ -0,0 +1,38 @@
+import React from "react";
+import TodoList from "./TodoList";
+import nextId from "react-id-generator";
+
+const Form = (props) => {
+    const htmlId=nextId();
+    const inputTextHandler = (e) => {
+        console.log(e.target.value);
+        props.setInputText(e.target.value);
+    };
+    const submitTodoHandler = (e) => {
+        e.preventDefault();
+        props.setTodos([
+            ...props.todos, {text: props.inputText, completed: false, id: htmlId}
+        ]);
+        props.setInputText("");
+    };
+    const statusHandler = (e) => {
+        props.setStatus(e.target.value);
+    }
+    return(
+    <form>
+      <input value={props.inputText} onChange={inputTextHandler} type="text" className="todo-input" />
+      <button onClick={submitTodoHandler} className="todo-button" type="submit">
+        <i className="fas fa-plus-square"></i>
+      </button>
+      <div className="select">
+        <select onChange={statusHandler} name="todos" className="filter-todo">
+          <option value="all">All</option>
+          <option value="completed">Completed</option>
+          <option value="uncompleted">Uncompleted</option>
+        </select>
+      </div>
+    </form>
+    );
+}
+
+export default Form;
